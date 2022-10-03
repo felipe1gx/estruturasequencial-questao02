af1eca9a4a13437ef434243ebcdd9fa23085ff79
felipe1gx <felipe1gx@gmail.com>
2022-10-03 15:58:30 -0300

    tentativa 2 de commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        Scanner console = new Scanner(System.in);
+        int n1;
+        System.out.println("Informe um número: ");
+        n1 = console.nextInt();
+        System.out.println("O número informado foi: " + n1);
+    }
+}
